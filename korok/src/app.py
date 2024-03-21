3dbdea603646ffb8bad74726eadc608604515fce
jonny <johnnybarda423@gmail.com>
2024-03-21 17:21:53 +0200

    add stuff

---
+++ korok/src/app.py
@@ -0,0 +1,52 @@
+import fastapi
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+
+from korok.src.config import get_settings
+
+
+def initialize_backend_application() -> fastapi.FastAPI:
+    app = fastapi.FastAPI(**settings.set_backend_app_attributes)  # type: ignore
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            str(origin) for origin in get_settings().security.BACKEND_CORS_ORIGINS
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=get_settings().security.ALLOWED_HOSTS,
+    )
+
+    # app.add_event_handler(
+    #     "startup",
+    #     execute_backend_server_event_handler(backend_app=app),
+    # )
+    # app.add_event_handler(
+    #     "shutdown",
+    #     terminate_backend_server_event_handler(backend_app=app),
+    # )
+
+    # app.include_router(router=router, prefix=settings.API_PREFIX)
+
+    return app
+
+
+backend_app: fastapi.FastAPI = initialize_backend_application()
+
+if __name__ == "__main__":
+    settings = get_settings()
+    uvicorn.run(
+        app="main:backend_app",
+        host=settings.server.SERVER_HOST,
+        port=settings.server.SERVER_PORT,
+        reload=settings.server.DEBUG,
+        workers=settings.server.SERVER_WORKERS,
+        log_level=settings.server.LOGGING_LEVEL,
+    )
---
+++ korok/src/config.py
@@ -0,0 +1,77 @@
+# File with environment variables and general configuration logic.
+# Env variables are combined in nested groups like "Security", "Database" etc.
+# So environment variable (case-insensitive) for jwt_secret_key will be "security__jwt_secret_key"
+#
+# Pydantic priority ordering:
+#
+# 1. (Most important, will overwrite everything) - environment variables
+# 2. `.env` file in root folder of project
+# 3. Default values
+#
+# "sqlalchemy_database_uri" is computed field that will create valid database URL
+#
+# See https://pydantic-docs.helpmanual.io/usage/settings/
+# Note, complex types like lists are read as json-encoded strings.
+
+
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic import AnyHttpUrl, BaseModel, SecretStr, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy.engine.url import URL
+
+PROJECT_DIR = Path(__file__).parent.parent.parent
+
+
+class Security(BaseModel):
+    ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1"]
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
+
+
+class Server(BaseModel):
+    SERVER_HOST: str = "localhost"
+    SERVER_PORT: int = 8080
+    SERVER_WORKERS: int = 1
+    API_PREFIX: str = "/api"
+    DOCS_URL: str = "/docs"
+    OPENAPI_URL: str = "/openapi.json"
+    REDOC_URL: str = "/redoc"
+    OPENAPI_PREFIX: str = ""
+
+
+class Database(BaseModel):
+    HOSTNAME: str = "postgres"
+    USERNAME: str = "postgres"
+    PASSWORD: SecretStr
+    PORT: int = 5432
+    DB: str = "postgres"
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def sqlalchemy_uri(self) -> URL:
+        return URL.create(
+            drivername="postgresql+asyncpg",
+            username=self.USERNAME,
+            password=self.PASSWORD.get_secret_value(),
+            host=self.HOSTNAME,
+            port=self.PORT,
+            database=self.DB,
+        )
+
+
+class Settings(BaseSettings):
+    security: Security
+    server: Server
+    database: Database
+
+    model_config = SettingsConfigDict(
+        env_file=f"{PROJECT_DIR}/.env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+    )
+
+
+@lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    return Settings()  # type: ignore
